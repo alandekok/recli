b2b73e0ce3e4d8ff1ec49645770f0eabeb3371c6
Alan T. DeKok <aland@freeradius.org>
2011-09-20 14:38:55 +0200

    Add example of string parsing

--- example.c
+++ example.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "linenoise.h"
 
+static int in_string = 0;
+static size_t string_start = 0;
 
 void completion(const char *buf, size_t len, linenoiseCompletions *lc) {
     if (buf[0] == 'h') {
@@ -11,12 +13,45 @@ void completion(const char *buf, size_t len, linenoiseCompletions *lc) {
 }
 
 int foundspace(const char *buf, size_t len, char c) {
-  if (len == 0) return 1;
+    if (in_string) return 0;
 
-  if (buf[len -1] == c) return 1;
+    if (len == 0) return 1;
 
-  printf("\r\nSPACE!\r\n");
-  return 0;
+    if (buf[len -1] == c) return 1;
+
+    printf("\r\nSPACE!\r\n");
+    return 0;
+}
+
+int escapedquote(const char *start)
+{
+    while (*start) {
+        if (*start == '\\') {
+	    if (!start[1]) return 1;
+	    start += 2;
+	}
+	start++;
+    }
+    return 0;
+}
+
+
+int foundquote(const char *buf, size_t len, char c) {
+    if (!in_string) {
+        in_string = 1;
+	string_start = len;
+	return 0;
+    }
+
+    if (buf[string_start] != c) return 0;
+
+    if (escapedquote(buf + string_start)) return 0;
+
+    in_string = 0;
+    printf("\r\nSTRING %s%c\r\n", buf + string_start, buf[string_start]);
+    string_start = 0;
+
+    return 0;
 }
 
 int main(void) {
@@ -25,6 +60,8 @@ int main(void) {
     linenoiseSetCompletionCallback(completion);
     linenoiseHistoryLoad("history.txt"); /* Load the history at startup */
     linenoiseSetCharacterCallback(foundspace, ' ');
+    linenoiseSetCharacterCallback(foundquote, '"');
+    linenoiseSetCharacterCallback(foundquote, '\'');
 
     while((line = linenoise("hello> ")) != NULL) {
         if (line[0] != '\0') {
