9d3b18b3459d1192f691d73e257c9b604b38d40a
Alan T. DeKok <aland@freeradius.org>
2016-01-19 13:53:32 -0500

    We may have many concat nodes for help

--- src/syntax.c
+++ src/syntax.c
@@ -3046,6 +3046,8 @@ const char *syntax_show_help(cli_syntax_t *head, int argc, char *argv[])
 		assert(a->type == CLI_TYPE_CONCAT);
 		assert(b->type == CLI_TYPE_EXACT);
 
+		if (strcmp(argv[i], (char *) b->first) != 0) break;
+
 		a = a->next;
 	}
 
@@ -3103,6 +3105,8 @@ int syntax_print_context_help(cli_syntax_t *head, int argc, char *argv[])
 		b = a->first;
 		assert(a->type == CLI_TYPE_CONCAT);
 		assert(b->type == CLI_TYPE_EXACT);
+
+		if (strcmp(argv[i], (char *) b->first) != 0) break;
 
 		a = a->next;
 	}
